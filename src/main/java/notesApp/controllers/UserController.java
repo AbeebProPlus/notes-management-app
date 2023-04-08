e946e6c561ccb3e40d3204c0a4bb49a8b54614a9
AbeebProPlus <68737847+AbeebProPlus@users.noreply.github.com>
2023-04-09 00:17:45 +0100

    Updated UserController component

--- src/main/java/notesApp/controllers/UserController.java
+++ src/main/java/notesApp/controllers/UserController.java
@@ -7,10 +7,15 @@ import notesApp.dtos.requests.UserRegistrationRequest;
 import notesApp.dtos.responses.UserLoginResponse;
 import notesApp.dtos.responses.UserRegistrationResponse;
 import notesApp.services.UserService;
+import notesApp.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import java.time.ZonedDateTime;
+
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -19,20 +24,52 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public UserRegistrationResponse register(@RequestBody UserRegistrationRequest registrationRequest){
-        return userService.register(registrationRequest);
+    public ResponseEntity<ApiResponse> register(@RequestBody UserRegistrationRequest registrationRequest,
+                                             HttpServletRequest httpServletRequest){
+        ApiResponse response = ApiResponse.builder()
+                .data(userService.register(registrationRequest))
+                .statusCode(HttpStatus.OK)
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
     @PostMapping("/login")
-    public UserLoginResponse login(@RequestBody UserLoginRequest loginRequest){
-        return userService.login(loginRequest);
+    public ResponseEntity<ApiResponse> login(@RequestBody UserLoginRequest loginRequest,
+                                   HttpServletRequest httpServletRequest){
+        ApiResponse response = ApiResponse.builder()
+                .data(userService.login(loginRequest))
+                .statusCode(HttpStatus.OK)
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
     @GetMapping("/notes/{userName}")
-    public List<Note> getAllNotes(@PathVariable String userName){
-        return userService.getAllNotes(userName);
+    public ResponseEntity<ApiResponse> getUserNotes(@PathVariable String userName, HttpServletRequest httpServletRequest){
+       // return userService.getAllNotes(userName);
+        ApiResponse response = ApiResponse.builder()
+                .data(userService.getAllNotes(userName))
+                .statusCode(HttpStatus.OK)
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/noteTitle")
-    public List<Note> findNotesByTitle(@RequestBody FindUserNoteByTitleRequest findUserNoteByTitleRequest){
-        return userService.getUserNotesByTitle(findUserNoteByTitleRequest);
+    public ResponseEntity<ApiResponse> findNotesByTitle(@RequestBody FindUserNoteByTitleRequest findUserNoteByTitleRequest,
+                                       HttpServletRequest httpServletRequest){
+        ApiResponse response = ApiResponse.builder()
+                .data( userService.getUserNotesByTitle(findUserNoteByTitleRequest))
+                .statusCode(HttpStatus.OK)
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
