6139e2623c51ba3af4908d4faf43b117374b8ab2
AbeebProPlus <68737847+AbeebProPlus@users.noreply.github.com>
2023-04-09 00:19:03 +0100

    Updated NoteController RestContoller class

--- src/main/java/notesApp/controllers/NoteController.java
+++ src/main/java/notesApp/controllers/NoteController.java
@@ -7,8 +7,14 @@ import notesApp.dtos.responses.CreateNoteResponse;
 import notesApp.dtos.responses.DeleteNoteResponse;
 import notesApp.dtos.responses.EditNoteResponse;
 import notesApp.services.NoteService;
+import notesApp.utils.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.ZonedDateTime;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -17,15 +23,39 @@ public class NoteController {
     private NoteService noteService;
 
     @PostMapping("/create_note")
-    public CreateNoteResponse createNote(@RequestBody CreateNoteRequest createNoteRequest){
-        return noteService.createNote(createNoteRequest);
+    public ResponseEntity<ApiResponse> createNote(@RequestBody CreateNoteRequest createNoteRequest,
+                                                         HttpServletRequest httpServletRequest){
+        ApiResponse response = ApiResponse.builder()
+                .data(noteService.createNote(createNoteRequest))
+                .statusCode(HttpStatus.OK)
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
     @DeleteMapping("/delete_note")
-    public DeleteNoteResponse deleteNote(@RequestBody DeleteNoteRequest deleteNoteRequest){
-        return noteService.deleteNote(deleteNoteRequest);
+    public ResponseEntity<ApiResponse> deleteNote(@RequestBody DeleteNoteRequest deleteNoteRequest,
+                                         HttpServletRequest httpServletRequest){
+        ApiResponse response = ApiResponse.builder()
+                .data(noteService.deleteNote(deleteNoteRequest))
+                .statusCode(HttpStatus.OK)
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
     @PatchMapping("/patch_note")
-    public EditNoteResponse editNote(@RequestBody EditNoteRequest editNoteRequest){
-        return noteService.editNote(editNoteRequest);
+    public ResponseEntity<ApiResponse> editNote(@RequestBody EditNoteRequest editNoteRequest,
+                                                HttpServletRequest httpServletRequest){
+        ApiResponse response = ApiResponse.builder()
+                .data(noteService.editNote(editNoteRequest))
+                .statusCode(HttpStatus.OK)
+                .timeStamp(ZonedDateTime.now())
+                .path(httpServletRequest.getRequestURI())
+                .isSuccessful(true)
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
