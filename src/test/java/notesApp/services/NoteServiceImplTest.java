507baac465131a3ce6e9ce2aa0c8f23e7cdc0646
AbeebProPlus <68737847+AbeebProPlus@users.noreply.github.com>
2023-04-09 01:44:40 +0100

    Update NoteServiceImplTest.java

--- src/test/java/notesApp/services/NoteServiceImplTest.java
+++ src/test/java/notesApp/services/NoteServiceImplTest.java
@@ -1,17 +1,22 @@
 package notesApp.services;
 
+import notesApp.data.models.Note;
+import notesApp.data.models.User;
 import notesApp.data.repositories.NoteRepository;
-import notesApp.dtos.requests.CreateNoteRequest;
-import notesApp.dtos.requests.DeleteNoteRequest;
-import notesApp.dtos.requests.EditNoteRequest;
+import notesApp.dtos.requests.*;
 import notesApp.dtos.responses.CreateNoteResponse;
 import notesApp.dtos.responses.DeleteNoteResponse;
 import notesApp.dtos.responses.EditNoteResponse;
+import notesApp.dtos.responses.UserRegistrationResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.naming.ldap.PagedResultsControl;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
@@ -22,6 +27,8 @@ public class NoteServiceImplTest {
     private NoteService noteService;
     @Autowired
     private NoteRepository noteRepository;
+    @Autowired
+    private UserService userService;
     private CreateNoteRequest createNoteRequest;
     private EditNoteRequest editNoteRequest;
     private DeleteNoteRequest deleteNoteRequest;
@@ -31,38 +38,98 @@ public class NoteServiceImplTest {
         createNoteRequest = new CreateNoteRequest();
         editNoteRequest = new EditNoteRequest();
         deleteNoteRequest = new DeleteNoteRequest();
-
-        createNoteRequest.setUserName("mac8ver26");
-        createNoteRequest.setTitle("This is a title");
-        createNoteRequest.setContent("This is a content");
-        createNoteRequest.setEditStatus("");
-
-        editNoteRequest.setNoteId("63a72b79ef475662cd7b9c6f");
-        editNoteRequest.setTitle("Edited note title");
-        editNoteRequest.setContent("Edited note content");
-        editNoteRequest.setUserName(createNoteRequest.getUserName());
-
-        deleteNoteRequest.setNoteId("63ac6b55980baa40273a20f7");
-        deleteNoteRequest.setUserName("mac8ver26");
     }
 
     @Test
     @DisplayName("Test that a registered user can create a note")
     void testThatANoteCanBeCreated(){
-        CreateNoteResponse response = noteService.createNote(createNoteRequest);
-        assertEquals(201, response.getStatusCode());
+       UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+       userRegistrationRequest.setUserName("habeeb");
+       userRegistrationRequest.setEmail("habeeb@gmail.com");
+       userRegistrationRequest.setPassword("hab4sure");
+       userService.register(userRegistrationRequest);
+
+       createNoteRequest.setUserName(userRegistrationRequest.getUserName());
+       createNoteRequest.setTitle("This is a title");
+       createNoteRequest.setContent("This is a content");
+
+       CreateNoteResponse response = noteService.createNote(createNoteRequest);
+       assertEquals(200, response.getStatusCode());
+
     }
     @Test
     @DisplayName("Test that a user can edit a note")
     void testThatANoteCanBeEdited(){
-        EditNoteResponse response = noteService.editNote(editNoteRequest);
-        assertEquals(201, response.getStatusCode());
+        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+        userRegistrationRequest.setUserName("habb");
+        userRegistrationRequest.setEmail("habb@gmail.com");
+        userRegistrationRequest.setPassword("hab4sure");
+        userService.register(userRegistrationRequest);
+
+        createNoteRequest.setUserName(userRegistrationRequest.getUserName());
+        createNoteRequest.setTitle("This is a title");
+        createNoteRequest.setContent("This is a content");
+
+        CreateNoteResponse response = noteService.createNote(createNoteRequest);
+        assertEquals(200, response.getStatusCode());
+
+        editNoteRequest.setNoteId(response.getNote().getId());
+        editNoteRequest.setTitle("Edited note title");
+        editNoteRequest.setContent("Edited note content");
+        editNoteRequest.setUserName(createNoteRequest.getUserName());
+        EditNoteResponse editNoteResponse = noteService.editNote(editNoteRequest);
+        assertEquals("Edited note title", editNoteResponse.getNote().getTitle());
+        assertEquals("Edited note content", editNoteResponse.getNote().getContent());
     }
-//    @Test
-//    @DisplayName("Test that a user can delete a note")
-//    void testThatANoteCanBeDeleted(){
-//        DeleteNoteResponse response = noteService.deleteNote(deleteNoteRequest);
-//        assertEquals(201, response.getStatusCode());
-//        assertNull(noteRepository.findNoteById("63a72b79ef475662cd7b9c6f"));
-//    }
+    @Test
+    @DisplayName("Test that a user can delete a note")
+    void testThatANoteCanBeDeleted(){
+        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+        userRegistrationRequest.setUserName("habbeebb");
+        userRegistrationRequest.setEmail("habbeebb@gmail.com");
+        userRegistrationRequest.setPassword("hab4sure");
+        userService.register(userRegistrationRequest);
+
+
+        createNoteRequest.setUserName(userRegistrationRequest.getUserName());
+        createNoteRequest.setTitle("This is a newww title");
+        createNoteRequest.setContent("This is a neww content");
+
+        CreateNoteResponse response = noteService.createNote(createNoteRequest);
+        assertEquals(200, response.getStatusCode());
+
+        DeleteNoteRequest deleteNoteRequest1 = new DeleteNoteRequest();
+        deleteNoteRequest1.setNoteId(response.getNote().getId());
+        deleteNoteRequest1.setUserName(userRegistrationRequest.getUserName());
+        noteService.deleteNote(deleteNoteRequest1);
+
+        FindUserNoteByTitleRequest findUserNoteByTitleRequest = new FindUserNoteByTitleRequest();
+        findUserNoteByTitleRequest.setUserName(userRegistrationRequest.getUserName());
+        findUserNoteByTitleRequest.setNoteTitle("This is a neww title");
+        assertEquals(0, userService.getUserNotesByTitle(findUserNoteByTitleRequest).size());
+    }
+    @Test
+    @DisplayName("Test that user can get note(s) by title")
+    void testThatNoteCanBeGottenByTiTle(){
+        UserRegistrationRequest userRegistrationRequest = new UserRegistrationRequest();
+        userRegistrationRequest.setUserName("Mellow");
+        userRegistrationRequest.setEmail("mellow@gmail.com");
+        userRegistrationRequest.setPassword("hab4sure");
+        userService.register(userRegistrationRequest);
+
+
+        createNoteRequest.setUserName(userRegistrationRequest.getUserName());
+        createNoteRequest.setTitle("This is a neww title");
+        createNoteRequest.setContent("This is a neww content");
+
+        CreateNoteResponse response = noteService.createNote(createNoteRequest);
+        assertEquals(200, response.getStatusCode());
+
+        FindUserNoteByTitleRequest findUserNoteByTitleRequest = new FindUserNoteByTitleRequest();
+        findUserNoteByTitleRequest.setUserName(userRegistrationRequest.getUserName());
+        findUserNoteByTitleRequest.setNoteTitle(createNoteRequest.getTitle());
+        List<Note> foundNote = userService.getUserNotesByTitle(findUserNoteByTitleRequest);
+        assertEquals("This is a neww title", foundNote.get(0).getTitle());
+        assertEquals("This is a neww content", foundNote.get(0).getContent());
+    }
 }
